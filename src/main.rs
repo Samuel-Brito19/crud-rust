0edb7a221081df945ca7edc0eb9383c54f489dd2
Samuel-Brito19 <sambriton7@gmail.com>
2024-10-30 16:55:45 -0300

    Setting up diesel

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use axum::{routing::get, Router};
+mod user;
+use axum::{routing::get, Error, Router};
+use user::User;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/users", get(get_users()));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_users() -> Result<Vec<User>, Error> {
+    todo!()
 }
---
+++ src/user.rs
@@ -0,0 +1,7 @@
+pub struct User {
+    name: String,
+    email: String,
+    password: String,
+    age: u32
+}
+
