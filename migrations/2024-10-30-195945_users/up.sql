0876fb8df16488b993b64b3576498f5c37f9f999
Samuel-Brito19 <sambriton7@gmail.com>
2024-10-30 18:01:11 -0300

    SQL files

---
+++ migrations/2024-10-30-195945_users/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    age INTEGER
+)
